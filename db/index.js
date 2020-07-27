4e257a0d541e41569a1bdd92932e7db3e7c3122b
deosama77 <deosama77@gmail.com>
2020-07-27 09:55:29 +0300

    updated

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 try {
     let pgp = require("pg-promise")({})
-    let db = pgp("postgres://osama:123456@localhost:5432/osama")
+    // let db = pgp("postgres://osama:123456@localhost:5432/osama")
+     let db=pgp(process.env.DATABASE_URL)
     module.exports=db;
 } catch (e) {
     console.log("Error connection with data base : ",e)
