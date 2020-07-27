0675606e30ee5e07132e2bfa6c50627c2662a594
deosama77 <deosama77@gmail.com>
2020-07-27 17:18:30 +0300

    updated

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ try {
     let pgp = require("pg-promise")({})
     // let db = pgp("postgres://osama:123456@localhost:5432/osama")
     //  let db=pgp(process.env.DATABASE_URL);
-    let db=`postgres://azphehlnchufkc:3a9f36012cf86b3562a52a2d30ae757e8023c57bf54054168da28347264ecf4b@ec2-54-197-254-117.compute-1.amazonaws.com:5432/dse6jvnetdig6`
+    let db=`postgres://tvfvfqohbubrxj:a39bfebdc20c9e84ff2048f6acad1483966f5b9691f49386355aad789f80fcbe@ec2-54-197-254-117.compute-1.amazonaws.com:5432/dfmb3cc1jtpr9f`
     console.log("Database",process.env.DATABASE_URL)
     module.exports=db;
 } catch (e) {
